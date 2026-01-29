6af88c4a9ded54b11bf6fb10637dadce35c89e9d
Sandra <su222fd@student.lnu.se>
2026-01-29 14:15:31 +0100

    animal + sheep + wolf

--- src/main/java/simulator/model/Animal.java
+++ src/main/java/simulator/model/Animal.java
@@ -1,5 +1,7 @@
 package simulator.model;
 
+import org.json.JSONObject;
+
 import simulator.misc.Vector2D;
 
 public abstract class Animal implements Entity, AnimalInfo{
@@ -18,6 +20,86 @@ public abstract class Animal implements Entity, AnimalInfo{
     protected AnimalMapView regionMngr;
     protected SelectionStrategy mateStrategy;
 
+    protected Animal(String geneticCode, Diet diet, double sightRange, double initSpeed, SelectionStrategy mateStrategy, Vector2D pos){
+        this.geneticCode = geneticCode;
+        this.diet = diet;
+        this.sightRange = sightRange;
+        this.pos = pos; //pos puede ser null y en ese caso se inicializa a un valor aleatorio en el método init
+        this.mateStrategy = mateStrategy;
+        this.speed = Utils.getRandomizedParameter(initSpeed, 0.1);
+        this.state = State.NORMAL;
+        this.energy = 100.0; //crear constantes?
+        this.desire = 0.0;
+        this.dest = null;
+        this.mateTarget = null;
+        this.baby = null;
+        this.regionMngr = null;
+        //geneticCode tiene que ser una cadena de caracteres no vacía, sightRange y initSpeed números positivos y mateStrategy no es null. Hay que lanzar una excepción correspondiente con un mensaje informativo si algún valor es incorrecto (p.ej., IllegalArgumentException).
+    }
 
+    protected Animal(Animal p1, Animal p2){
+        this.dest = null;
+        this.baby = null;
+        this.mateTarget = null;
+        this.regionMngr = null;
+        this.state = State.NORMAL;
+        this.desire = 0.0;
+        this.geneticCode = p1.geneticCode;
+        this.diet = p1.diet;
+        this.mateStrategy = p2.mateStrategy;
+        this.energy = (p1.energy + p2.energy)/2;
+        this.pos = p1.getPosition().plus(Vector2D.getRandomVector(-1,1).scale(60.0*(Utils.RAND.nextGaussian()+1)));
+        this.sightRange = Utils.getRandomizedParameter((p1.getSightRange()+p2.getSightRange())/2,0.2);
+        this.speed = Utils.getRandomizedParameter((p1.getSpeed()+p2.getSpeed())/2, 0.2);
+    }
+
+    void init(AnimalMapView regMngr){
+        this.regionMngr = regMngr;
+        //si pos es null hay que elegir una posición aleatoria dentro del rango del mapa (X entre 0 y regionMngr.getWidth()-1 e Y entre 0 y regionMngr.getHeight()-1). Si pos no es null hay que ajustarlo para que esté dentro del mapa si es necesario (ver el apartado Ajustar posiciones).
+        //Elegir una posición aleatoria para dest (dentro del rango del mapa).
+    }
+
+    Animal deliverBaby(){
+        return this.baby;
+        this.baby = null;
+    }
+
+    protected void move(double speed){
+        this.pos = pos.plus(dest.minus(pos).direction().scale(speed));
+    }
+
+    protected void setState(State state){
+        this.state = state;
+        switch (state) {
+        case NORMAL:
+            setNormalStateAction();
+            break;
+        case HUNGER:
+            setHungerStateAction();
+            break;
+        case MATE:
+            setMateStateAction();
+            break;
+        case DEAD:
+            setDeadStateAction();
+            break;
+        case DANGER:
+            setDangerStateAction();
+            break;
+        }
+    }
+
+    abstract protected void setNormalStateAction();
+    abstract protected void setMateStateAction();
+    abstract protected void setHungerStateAction();
+    abstract protected void setDangerStateAction();
+    abstract protected void setDeadStateAction();
+
+    public JSONObject asJSON(){
+        "pos": [28.90696391797469,22.009772194487613],
+        "gcode": "Sheep",
+        "diet": "HERBIVORE",
+        "state": "NORMAL"
+    }
 
 }
---
+++ src/main/java/simulator/model/Wolf.java
@@ -0,0 +1,123 @@
+package simulator.model;
+
+import simulator.misc.Vector2D;
+
+public class Wolf extends Animal{
+    //Es un animal carnívoro con código genético "Wolf". Es un animal que caza a otros animales herbívoros y también puede comer lo que proporciona la región en la que está, y puede emparejarse con otros animales con el mismo código genético.
+    private Animal huntTarget;
+    private SelectionStrategy huntingStrategy;
+
+    public Wolf(SelectionStrategy mateStrategy, SelectionStrategy huntingStrategy,  Vector2D pos){
+        //recibe las estrategias y la posición y simplemente le almacena en los atributos correspondientes (llamando a la constructora de la superclase). 
+        this.sightRange = 50.0;
+        this.speed = 60.0;
+    }
+
+    protected Wolf(Wolf p1, Animal p2){
+        super(p1, p2);
+        this.huntingStrategy = p1.huntingStrategy;
+        this.huntTarget = null;
+    }
+    @Override
+    public void update(double dt) {
+        if(this.state == State.DEAD){
+            return;
+        }
+        //Actualizar el objeto según el estado del animal (ver la descripción abajo).
+        if(){
+            //Si la posición está fuera del mapa, ajustarla y cambiar su estado a NORMAL.
+            this.state = State.NORMAL;
+        }
+        if(this.energy == 0.0 || this.age > 14.0){
+            this.state = State.DEAD;
+        }
+        if(this.state != State.DEAD){ //not sure
+            double newEnergy = this.energy + getFood(this, dt);
+            if(newEnergy < 100.0 && newEnergy > 0){
+                this.energy = newEnergy;
+            }
+        }
+    }
+
+        @Override
+    public State getState() {
+        return this.state;
+    }
+
+    @Override
+    public Vector2D getPosition() {
+        return this.pos;
+    }
+
+    @Override
+    public String getGeneticCode() {
+        return this.geneticCode;
+    }
+
+    @Override
+    public Diet getDiet() {
+        return this.diet;
+    }
+
+    @Override
+    public double getSpeed() {
+        return this.speed;
+    }
+
+    @Override
+    public double getSightRange() {
+        return this.sightRange;
+    }
+
+    @Override
+    public double getEnergy() {
+        return this.energy;
+    }
+
+    @Override
+    public double getAge() {
+        return this.age;
+    }
+
+    @Override
+    public Vector2D getDestination() {
+        return this.dest;
+    }
+
+    @Override
+    public boolean isPregnant() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isPregnant'");
+    }
+
+    @Override
+    protected void setNormalStateAction() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setNormalStateAction'");
+    }
+
+    @Override
+    protected void setMateStateAction() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setMateStateAction'");
+    }
+
+    @Override
+    protected void setHungerStateAction() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setHungerStateAction'");
+    }
+
+    @Override
+    protected void setDangerStateAction() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setDangerStateAction'");
+    }
+
+    @Override
+    protected void setDeadStateAction() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setDeadStateAction'");
+    }
+
+}
