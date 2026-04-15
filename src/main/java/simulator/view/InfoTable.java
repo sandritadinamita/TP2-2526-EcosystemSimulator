a6cbdc55eb349d1cc4028d4400ef7c089b43aa1c
albafuer <albafuer@ucm.es>
2026-04-15 20:00:27 +0200

    infoTable hecho

--- src/main/java/simulator/view/InfoTable.java
+++ src/main/java/simulator/view/InfoTable.java
@@ -2,6 +2,7 @@ package simulator.view;
 
 import javax.swing.*;
 import javax.swing.table.TableModel;
+import java.awt.BorderLayout;
 
 
 public class InfoTable extends JPanel {
@@ -16,9 +17,14 @@ public class InfoTable extends JPanel {
   }
 
   private void initGUI() {
-    // TODO cambiar el layout del panel a BorderLayout()
-    // TODO añadir un borde con título al JPanel, con el texto this.title
+    // TODO cambiar el layout del panel a BorderLayout() (hecho)
+      this.setLayout(new BorderLayout());
+    // TODO añadir un borde con título al JPanel, con el texto this.titl (hecho)
+    this.setBorder(BorderFactory.createTitledBorder(this.title));
     // TODO añadir un JTable (con barra de desplazamiento vertical) que use
-    //      this.tableModel
+    //      this.tableModel (hecho)
+    JTable table = new JTable(this.tableModel);
+    JScrollPane scrollPane = new JScrollPane(table);
+    this.add(scrollPane, BorderLayout.CENTER);
   }
 }
