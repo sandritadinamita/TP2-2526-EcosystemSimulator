1efe803710dbd70cc7c46325974a2dc85d9c83ab
albafuer <albafuer@ucm.es>
2026-04-16 11:35:00 +0200

    MapWindow (creo que está bien)

--- src/main/java/simulator/view/MapWindow.java
+++ src/main/java/simulator/view/MapWindow.java
@@ -2,10 +2,10 @@ package simulator.view;
 
 import java.awt.BorderLayout;
 import java.awt.Frame;
+import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.util.List;
-
-import javax.swing.JFrame;
+import java.util.Map;
 
 import simulator.control.Controller;
 import simulator.model.AnimalInfo;
@@ -26,56 +26,82 @@ class MapWindow extends JFrame implements EcoSysObserver {
     this.ctrl = ctrl;
     this.parent = parent;
     intiGUI();
-    // TODO registrar this como observador
+    ctrl.addObserver(this); 
+    // TODO registrar this como observador (hecho)
   }
 
   private void intiGUI() {
     JPanel mainPanel = new JPanel(new BorderLayout());
-    // TODO poner contentPane como mainPanel
-
-    // TODO crear el viewer y añadirlo a mainPanel (en el centro)
-
+    // TODO poner contentPane como mainPanel (hecho)
+    this.setContentPane(mainPanel);
+    // TODO crear el viewer y añadirlo a mainPanel (en el centro)(hecho)
+    MapViewer viewer = new MapViewer();
+    mainPanel.add(viewer, BorderLayout.CENTER);
     // TODO en el método windowClosing, eliminar ‘MapWindow.this’ de los
-    //      observadores
-    addWindowListener(new WindowListener() { ... });
+    //      observadores(hecho)
+  
+    addWindowListener(new WindowListener() {
 
+      @Override
+      public void windowOpened(WindowEvent e) {
+      }
+
+      @Override
+      public void windowClosing(WindowEvent e) {
+       ctrl.removeObserver(MapWindow.this);
+      }
+
+      @Override
+      public void windowClosed(WindowEvent e) {
+      }
+
+      @Override
+      public void windowIconified(WindowEvent e) {
+      }
+
+      @Override
+      public void windowDeiconified(WindowEvent e) {
+      }
+
+      @Override
+      public void windowActivated(WindowEvent e) {
+      }
+
+      @Override
+      public void windowDeactivated(WindowEvent e) {
+      }
+
+    });
     pack();
     if (this.parent != null)
       setLocation(
         this.parent.getLocation().x + parent.getWidth() / 2 - getWidth() / 2,
         this.parent.getLocation().y + parent.getHeight() / 2 - getHeight() / 2);
-      setResizable(false);
-      setVisible(true);
+    setResizable(false);
+    setVisible(true);
   }
 
   @Override
   public void onRegister(double time, MapInfo map, List<AnimalInfo> animals) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'onRegister'");
+    SwingUtilities.invokeLater(() -> { this.viewer.reset(time, map, animals); pack(); });
   }
 
   @Override
   public void onReset(double time, MapInfo map, List<AnimalInfo> animals) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'onReset'");
+    SwingUtilities.invokeLater(() -> { this.viewer.reset(time, map, animals); pack(); });
   }
 
   @Override
   public void onAnimalAdded(double time, MapInfo map, List<AnimalInfo> animals, AnimalInfo a) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'onAnimalAdded'");
   }
 
   @Override
   public void onRegionSet(int row, int col, MapInfo map, RegionInfo r) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'onRegionSet'");
+
   }
 
   @Override
   public void onAdvance(double time, MapInfo map, List<AnimalInfo> animals, double dt) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'onAdvance'");
+    SwingUtilities.invokeLater(() -> { this.viewer.update(animals, dt); });
   }
-  // TODO otros métodos van aquí….
 }
