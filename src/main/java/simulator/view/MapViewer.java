1cf743b054ad6ea1056caa6ac241e52e26f46546
Sandra <su222fd@student.lnu.se>
2026-04-17 12:50:47 +0200

    mapviewer

--- src/main/java/simulator/view/MapViewer.java
+++ src/main/java/simulator/view/MapViewer.java
@@ -118,7 +118,21 @@ public class MapViewer extends AbstractMapViewer {
 					//      comes the first element of Animal.State.values(), and after the last of
 					//      these values comes null.
 					//
+					if (currentState == null) {
+						currentState = State.values()[0];
+					} else {
+						int i = 0;
+						while (i < State.values().length && State.values()[i] != currentState) {
+							i++;
+						}
+						if (i == State.values().length - 1) {
+							currentState = null;
+						} else {
+							currentState = State.values()[i + 1];
+						}
+					}
 					repaint();
+					break;
 				default:
 				}
 			}
@@ -187,6 +201,11 @@ public class MapViewer extends AbstractMapViewer {
 		//
 		// h: toggle help
 		// s: show animals of a specific state
+		if(showHelp){
+			g.setColor(Color.RED);
+			g.drawString("h: toggle help", 10, 20);
+			g.drawString("s: show animals of a specific state", 10, 40);
+		}
 
 	}
 
@@ -197,7 +216,12 @@ public class MapViewer extends AbstractMapViewer {
 		//      return true of the animal is visible, i.e., currState is null or its
 		//      state is equal to currState.
 		//
-		return true;
+		if(currentState == null || a.getState() == currentState){
+			return true;
+		}
+		else{
+			return false;
+		}
 	}
 
 	private void drawObjects(Graphics2D g, Collection<AnimalInfo> animals, Double time) {
@@ -206,6 +230,16 @@ public class MapViewer extends AbstractMapViewer {
 		//
 		//      Draw a grid of regions.
 		//
+		g.setColor(Color.LIGHT_GRAY);
+		for (int i = 0; i <= cols; i++) {
+			int x = i * rWidth;
+			g.drawLine(x, 0, x, height);
+		}
+
+		for (int i = 0; i <= rows; i++) {
+			int y = i * rHeight;
+			g.drawLine(0, y, width, y);
+		}
 
 
 		// Dibujar los animales.
@@ -232,12 +266,17 @@ public class MapViewer extends AbstractMapViewer {
 			//
 			//      If espInfo is null, add a corresponding entry to the map. For the color
 			//      use ViewUtils.getColor(a.getGeneticCode()).
+			if(speciesInfo == null){
+				speciesInfo = new SpeciesInfo(ViewUtils.getColor(a.getGeneticCode()));
+    			kindsInfo.put(a.getGeneticCode(), speciesInfo);
+			}
 
 
 			// TODO Incrementar el contador de la especie (es decir el contador dentro de
 			//      speciesInfo).
 			//
 			//      Increment the counter of the species (i.e., the one inside speciesInfo).
+			speciesInfo.count++;
 
 			// TODO Dibujar el animal en la posición correspondiente, usando el color
 			//      speciesInfo.color. Su tamaño tiene que ser relativo a su edad, por ejemplo
@@ -246,18 +285,30 @@ public class MapViewer extends AbstractMapViewer {
 			//      Draw the animal at the corresponding position, using the color
 			//      speciesInfo.color. Its size should be relative to the animal's age, e.g.,
 			//      age/2+2. For drawing you can use fillRoundRect, fillRect or fillOval.
+			g.setColor(speciesInfo.color);
+
+			int size = (int) (a.getAge() / 2 + 2);
+			int x = (int) a.getPosition().getX();
+			int y = (int) a.getPosition().getY();
+
+			g.fillOval(x, y, size, size);
 		}
 
 		// TODO Dibujar la etiqueta del estado visible, usando currState.toString(), si no
 		//      es null.
 		//
 		//      Draw the tag of the visible state, using currState.toString(), if it is not null.
+		if (currentState != null) {
+			drawStringWithRect(g, 10, height - 40, "State: " + currentState.toString());
+		}
 
 
 		// TODO Dibujar la etiqueta del tiempo. Para escribir solo 3 decimales puede
 		//      usar String.format("%.3f", time).
 		//
 		//      Draw the time. To use only 3 decimals you can use String.format("%.3f", time).
+		g.setColor(Color.MAGENTA);
+		drawStringWithRect(g, 10, height - 20, "Time: " + String.format("%.3f", time));
 
 
 		// TODO Dibujar la información de todas la especies. Al final de la iteración
@@ -266,7 +317,12 @@ public class MapViewer extends AbstractMapViewer {
 		//      Draw the information of each species. At the end of the iteration, reset the
 		//      species count.
 		//
+		int i = 2;
 		for (Entry<String, SpeciesInfo> e : kindsInfo.entrySet()) {
+			g.setColor(e.getValue().color);
+			drawStringWithRect(g, 10, 580 - 20*i, String.format("%s: %d", e.getKey(), e.getValue().count));
+			i++; 
+			e.getValue().count = 0;
 		}
 	}
 
@@ -287,10 +343,19 @@ public class MapViewer extends AbstractMapViewer {
 		//
 		//      Store objs and time in the corresponding fields, and call repaint() to
 		//      redraw the component.
+		this.objs = objs;
+		this.time = time;
+		repaint();
 	}
 
 	@Override
 	public void reset(double time, MapInfo map, List<AnimalInfo> animals) {
+		this.width = map.getWidth();
+		this.height = map.getHeight();
+		this.cols = map.getCols();
+		this.rows = map.getRows();
+		this.rWidth = width / cols;
+		this.rHeight = height / rows;
 		// TODO Actualizar los atributos width, height, cols, rows, etc.
 		//
 		//      Update the fields width, height, cols, rows, etc.
